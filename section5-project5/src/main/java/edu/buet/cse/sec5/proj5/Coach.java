e55eb45e6b4e83bc883f48c4bd299fda3bf115d7
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-01-06 17:42:01 +1100

    Retrieve injected literal values from properties file

---
+++ section5-project5/src/main/java/edu/buet/cse/sec5/proj5/Coach.java
@@ -0,0 +1,6 @@
+package edu.buet.cse.sec5.proj5;
+
+public interface Coach {
+  String getDailyWorkout();
+  String getDailyFortune();
+}
---
+++ section5-project5/src/main/java/edu/buet/cse/sec5/proj5/FortuneService.java
@@ -0,0 +1,5 @@
+package edu.buet.cse.sec5.proj5;
+
+public interface FortuneService {
+  String getFortune();
+}
