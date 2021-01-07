cb0079cfab9ab4979bfc0cafc5480dcd42565637
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-01-07 17:17:22 +1100

    Update spring boot version to 2.4.0

--- section4-project1/src/main/java/edu/buet/cse/sec4/proj1/MyApp.java
+++ section4-project1/src/main/java/edu/buet/cse/sec4/proj1/MyApp.java
@@ -6,7 +6,7 @@ public class MyApp {
 
   public static void main(String... args) {
     ClassPathXmlApplicationContext appContext =
-        new ClassPathXmlApplicationContext("/edu/buet/cse/sec4/proj1/applicationContext.xml");
+        new ClassPathXmlApplicationContext("applicationContext.xml");
     Coach myCoach = appContext.getBean("myCoach", Coach.class);
     System.out.println(myCoach.getDailyWorkout());
     appContext.close();
