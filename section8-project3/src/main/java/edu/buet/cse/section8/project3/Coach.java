2bc483704a37a157eaea187c32ccb6a9b2469b52
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-13 21:02:35 +1100

    Add a project to demonstrate setter injection

---
+++ section8-project3/src/main/java/edu/buet/cse/section8/project3/Coach.java
@@ -0,0 +1,6 @@
+package edu.buet.cse.section8.project3;
+
+public interface Coach {
+  String getDailyWorkout();
+  String getDailyFortune();
+}
---
+++ section8-project3/src/main/java/edu/buet/cse/section8/project3/FortuneService.java
@@ -0,0 +1,5 @@
+package edu.buet.cse.section8.project3;
+
+public interface FortuneService {
+  String getFortune();
+}
