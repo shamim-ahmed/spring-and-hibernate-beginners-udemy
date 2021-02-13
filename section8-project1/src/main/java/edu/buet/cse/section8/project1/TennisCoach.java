964e7134a8d7239aba7333bc124e5962504f1520
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-13 17:30:33 +1100

    Add a new project to demonstrate auto wiring feature

---
+++ section8-project1/src/main/java/edu/buet/cse/section8/project1/TennisCoach.java
@@ -0,0 +1,24 @@
+package edu.buet.cse.section8.project1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("myTennisCoach")
+public class TennisCoach implements Coach {
+  private FortuneService fortuneService;
+
+  @Autowired
+  public TennisCoach(FortuneService fortuneService) {
+    this.fortuneService = fortuneService;
+  }
+
+  @Override
+  public String getDailyWorkout() {
+    return "Practise serving for 1 hour";
+  }
+
+  @Override
+  public String getDailyFortune() {
+    return fortuneService.getFortune();
+  }
+}
