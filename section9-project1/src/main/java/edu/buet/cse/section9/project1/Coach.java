e0e5224d2d9050704f23128309e76df1ccdc8711
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-17 07:40:04 +1100

    Add a project to demonstrate the use of Scope annotation

---
+++ section9-project1/src/main/java/edu/buet/cse/section9/project1/Coach.java
@@ -0,0 +1,6 @@
+package edu.buet.cse.section9.project1;
+
+public interface Coach {
+  String getDailyWorkout();
+  String getDailyFortune();
+}
---
+++ section9-project1/src/main/java/edu/buet/cse/section9/project1/FortuneService.java
@@ -0,0 +1,5 @@
+package edu.buet.cse.section9.project1;
+
+public interface FortuneService {
+  String getFortune();
+}
