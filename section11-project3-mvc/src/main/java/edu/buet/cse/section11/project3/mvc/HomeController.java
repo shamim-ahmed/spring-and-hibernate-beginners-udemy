178aa27c05d6cb32c13bf604bc4a67a66ce143f2
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-21 21:18:43 +1100

    Add a project that demonstrates the use of InternalResourceViewResolver

---
+++ section11-project3-mvc/src/main/java/edu/buet/cse/section11/project3/mvc/HomeController.java
@@ -0,0 +1,16 @@
+package edu.buet.cse.section11.project3.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping(path = "/", method = RequestMethod.GET)
+  public String welcome(ModelMap modelMap) {
+    modelMap.addAttribute("username", "Clark Kent");
+    return "welcome";
+  }
+};
---
+++ section11-project3-mvc/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+
+<html>
+<body>
+  <h1>Hello world</h1>
+  <p>The username is ${username}</p>
+</body>
+</html>
