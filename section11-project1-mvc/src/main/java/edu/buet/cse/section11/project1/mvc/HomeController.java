f9e4c2788faad92cf6caf3a7ed71a712a034c211
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-21 22:04:59 +1100

    Minor modification

--- section11-project1-mvc/src/main/java/edu/buet/cse/section11/project1/mvc/HomeController.java
+++ section11-project1-mvc/src/main/java/edu/buet/cse/section11/project1/mvc/HomeController.java
@@ -1,16 +1,14 @@
 package edu.buet.cse.section11.project1.mvc;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-  
-  @RequestMapping("/")
-  public String index(HttpServletResponse response) {
-    response.setContentType("text/plain");
+
+  @RequestMapping(path = "/", method = RequestMethod.GET, produces = "text/plain")
+  public String index() {
     return "Hello world";
   }
 }
