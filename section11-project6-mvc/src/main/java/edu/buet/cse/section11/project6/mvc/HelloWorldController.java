083520397e40ce9c3d099fc29ccb3277ee26c3d1
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-02-27 08:57:01 +1100

    Add a project to demonstrate how Model object can be used for data transfer

---
+++ section11-project6-mvc/src/main/java/edu/buet/cse/section11/project6/mvc/HelloWorldController.java
@@ -0,0 +1,25 @@
+package edu.buet.cse.section11.project6.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+  @RequestMapping("/showForm")
+  public String showForm() {
+    return "helloWorld-form";
+  }
+
+  // note the object we use for transferring the result from controller to view
+  @RequestMapping("/processForm")
+  public String processForm(@RequestParam String studentName, Model model) {
+    String convertedName = studentName != null ? studentName.toUpperCase() : null;
+    String result = "Yo! " + convertedName;
+    model.addAttribute("message", result);
+
+    return "helloWorld";
+  }
+}
---
+++ section11-project6-mvc/src/main/webapp/WEB-INF/view/helloWorld.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<head>
+  <title>Result form</title>
+</head>
+<body>
+  <p>The message is <c:out value="${message}" /></p>
+</body>
