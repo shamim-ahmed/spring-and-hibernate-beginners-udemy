d5cec0a8f0076aa5663fb9040bca0b70e3c7e32d
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-03-27 10:06:42 +1100

    Add a project that implements one to one mapping between entities

---
+++ section23-project1-hibernate/src/main/java/edu/buet/cse/section23/project1/CreateInstructorDemo.java
@@ -0,0 +1,32 @@
+package edu.buet.cse.section23.project1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import edu.buet.cse.section23.project1.model.Instructor;
+import edu.buet.cse.section23.project1.model.InstructorDetail;
+
+public class CreateInstructorDemo {
+
+  public static void main(String... args) {
+    SessionFactory sessionFactory =
+        new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+            .addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
+    
+    try {
+      Transaction transaction = session.beginTransaction();
+      Instructor instructor = new Instructor("John", "Smith", "jsmith@gmail.com");
+      InstructorDetail instructorDetail = new InstructorDetail("https://www.youtube.com", "Philately");
+      instructor.setInstructorDetail(instructorDetail);
+      
+      session.save(instructor);
+      transaction.commit();
+    } finally {
+      session.close();
+      sessionFactory.close();
+    }
+  }
+}
---
+++ section23-project1-hibernate/src/main/java/edu/buet/cse/section23/project1/model/InstructorDetail.java
@@ -0,0 +1,64 @@
+package edu.buet.cse.section23.project1.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Instructor_detail")
+public class InstructorDetail {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "youtube_channel")
+  private String youtubeChannel;
+
+  @Column(name = "hobby")
+  private String hobby;
+
+  public InstructorDetail() {}
+
+  public InstructorDetail(String youtubeChannel, String hobby) {
+    this.youtubeChannel = youtubeChannel;
+    this.hobby = hobby;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getYoutubeChannel() {
+    return youtubeChannel;
+  }
+
+  public void setYoutubeChannel(String youtubeChannel) {
+    this.youtubeChannel = youtubeChannel;
+  }
+
+  public String getHobby() {
+    return hobby;
+  }
+
+  public void setHobby(String hobby) {
+    this.hobby = hobby;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("[").append("id = ").append(id).append(", youtubeChannel = ")
+        .append(youtubeChannel).append(", hobby = ").append(hobby).append("]");
+
+    return builder.toString();
+  }
+}
